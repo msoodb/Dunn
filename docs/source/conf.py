b1380a654d120a867c52979d4bbb9be3cc02fb69
msoodb <masoud.bolhassani@gmail.com>
2023-12-20 17:08:44 +0200

    msoodb-2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Dunn'
-copyright = '2023, msoodb'
-author = 'msoodb'
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
 
 release = '0.1'
 version = '0.1.0'
